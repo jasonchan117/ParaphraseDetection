fc4352d6b4ab0d4dd7b26229d0fed7bb12aacf31
Jasonchan_Co <18933818702@163.com>
2021-03-06 20:45:37 +0000

    Adding new annotations

--- utils.py
+++ utils.py
@@ -20,7 +20,7 @@ def getData(path):
 
     return data, label
 
-
+# Construct example and field objects.
 def get_dataset(data_x,label, text_field, label_field, data,test=False):
 
     fields = [("id", None),("text", text_field), ("label", label_field)]
@@ -36,26 +36,9 @@ def get_dataset(data_x,label, text_field, label_field, data,test=False):
 
 
 def binary_accuracy(preds, y):
-    """
-    Returns accuracy per batch, i.e. if you get 8/10 right, this returns 0.8, NOT 8
-    """
 
-    # .round函数 四舍五入，rounded_preds要么为0，要么为1
-    # neg为0, pos为1
     rounded_preds = torch.round(torch.sigmoid(preds))
-    
-    # convert into float for division
-    """
-    a = torch.tensor([1, 1])
-    b = torch.tensor([1, 1])
-    print(a == b)
-    output: tensor([1, 1], dtype=torch.uint8)
-  
-    a = torch.tensor([1, 0])
-    b = torch.tensor([1, 1])
-    print(a == b)
-    output: tensor([1, 0], dtype=torch.uint8)
-    """
+
     correct = (rounded_preds == y).float()
     acc = correct.sum() / len(correct)
 
@@ -67,62 +50,41 @@ def training(model, iterator, optimizer, criterion):
     epoch_acc = 0
     total_len = 0
 
-    # model.train()代表了训练模式
-    # model.train() ：启用 BatchNormalization 和 Dropout
-    # model.eval() ：不启用 BatchNormalization 和 Dropout
     model.train()
 
-    # iterator为train_iterator
     for batch in iterator:
-        # 梯度清零，加这步防止梯度叠加
+
         optimizer.zero_grad()
-
-        # batch.text 就是上面forward函数的参数text
-        # 压缩维度，不然跟 batch.label 维度对不上
-
         predictions = model(batch.text).squeeze(1)
 
         loss = criterion(predictions, batch.label)
 
         acc = binary_accuracy(predictions, batch.label)
 
-        loss.backward()  # 反向传播
-        optimizer.step()  # 梯度下降
-
-        # loss.item() 以及本身除以了 len(batch.label)
-        # 所以得再乘一次，得到一个batch的损失，累加得到所有样本损失
+        loss.backward()  
+        optimizer.step() 
         epoch_loss += loss.item() * len(batch.label)
-
-        # (acc.item(): 一个batch的正确率) * batch数 = 正确数
-        # train_iterator 所有batch的正确数累加
         epoch_acc += acc.item() * len(batch.label)
 
-        # 计算 train_iterator 所有样本的数量，应该是17500
         total_len += len(batch.label)
 
-    # epoch_loss / total_len ：train_iterator所有batch的损失
-    # epoch_acc / total_len ：train_iterator所有batch的正确率
     return epoch_loss / total_len, epoch_acc / total_len
 
-
-# 不用优化器了
 def evaluate(model, iterator, criterion):
     epoch_loss = 0
     epoch_acc = 0
     total_len = 0
 
-    # 转成测试模式，冻结dropout层或其他层
     model.eval()
 
     with torch.no_grad():
-        # iterator为valid_iterator
+
         ind=0
         sum_prec=0.
         sum_recall=0.
         sum_f1=0.
         sum_acc=0.
         for batch in iterator:
-            # 没有反向传播和梯度下降
 
             predictions = model(batch.text).squeeze(1)
             loss = criterion(predictions, batch.label)
@@ -139,8 +101,6 @@ def evaluate(model, iterator, criterion):
             total_len += len(batch.label)
 
             ind+=1
-
-    # 调回训练模式
     model.train()
 
     return epoch_loss / total_len, sum_acc/ind , sum_prec / ind, sum_recall / ind , sum_f1 / ind
