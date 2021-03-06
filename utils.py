bcd937476c6182a05d747e02591a748730f9ff78
Jasonchan_m <jasonchan@MacBook-Pro.local>
2021-03-07 01:42:41 +0800

    modified

--- utils.py
+++ utils.py
@@ -150,3 +150,5 @@ def epoch_time(start_time, end_time):
     elapsed_mins = int(elapsed_time / 60)
     elapsed_secs = int(elapsed_time - (elapsed_mins * 60))
     return elapsed_mins, elapsed_secs
+
+###
