3ade61ac3c9d13d10fd1d21f7625feeb6f18247f
Jasonchan_m <jasonchan@MacBook-Pro.local>
2021-03-07 15:51:44 +0800

    Compute the average value.

--- baseline.py
+++ baseline.py
@@ -8,7 +8,7 @@ vali_data, vali_label=getData('data/dev.data')
 res=[]
 for i in vali_data:
     if edit_distance(i[0],i[1]) >=34.919:
-        res.append(1)
-    else:
         res.append(-1)
+    else:
+        res.append(1)
 print(precision_score(vali_label, res), recall_score(vali_label, res), f1_score(vali_label, res),accuracy_score(vali_label, res))
